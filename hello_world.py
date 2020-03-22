22e02465e0f39ffa81014b2b92436188a42012e1
saad <saadbintariq444@gmail.com>
2020-03-22 12:14:28 +0500

    Second Commit

---
+++ hello_world.py
@@ -0,0 +1,3 @@
+print('I am Hello world from local machine to host')
+print('due to vagrant from host to localmachine &  from local machine')
+print('to host: synchronization works both ways i.e from both sides')
